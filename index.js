5458e9822b9afad3cc9ba2146a3deb3852513ea5
milanjacimovic <jacimovicmilanfon@gmail.com>
2021-03-10 00:04:18 +0100

    implemented bloxico search

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ async function automateSearch() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage("");
   await page.setViewport({ width: 1200, height: 800 });
-  await page.goto("https://www.google.rs");
+  await page.goto("https://www.google.com");
+  await page.type(".gLFyf.gsfi", "Bloxico");
+  await page.waitForTimeout(2000);
+  await page.click(".gNO89b");
 }
 
 main();
