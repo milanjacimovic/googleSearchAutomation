a039b0b1d08f7297a206d120da4e9935c15a0d38
milanjacimovic <jacimovicmilanfon@gmail.com>
2021-03-09 23:12:08 +0100

    implemented simple google load

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-let a = "hello world";
+const puppeteer = require("puppeteer");
+
+function main() {
+  automateSearch();
+}
+
+async function automateSearch() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage("");
+  await page.setViewport({ width: 1200, height: 800 });
+  await page.goto("https://www.google.rs");
+}
+
+main();
